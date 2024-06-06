530ee86d3322d57c4562cd0a0cd333bcbd8aea37
Iyasu2 <126699641+Iyasu2@users.noreply.github.com>
2024-06-06 21:12:39 +0300

    Update Resume.jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import CVIyasu_AsnakePDF from "./assets/Iyasu_Asnake_CV.pdf";
 import CVIyasu_Asnake1 from "./assets/image.jpg";
 import CVIyasu_AsnakePDF from "./assets/image.pdf";
 import "./Resume.css"; // Import your CSS file
